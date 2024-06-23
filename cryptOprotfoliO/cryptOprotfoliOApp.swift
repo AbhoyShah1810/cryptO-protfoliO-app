4c5a455eba2d2f974b26ad7b0487a2a41ef6b4bc
jellYfisH <jf.dev.1810@gmail.com>
2024-06-23 20:18:26 +0530

    Initial Commit

---
+++ cryptOprotfoliO/cryptOprotfoliOApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptOprotfoliOApp.swift
+//  cryptOprotfoliO
+//
+//  Created by Abhoy Prasad Shah on 23/06/24.
+//
+
+import SwiftUI
+
+@main
+struct cryptOprotfoliOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
